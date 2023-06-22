950a9589abe0bdacbe440805677dda3592f9e4ef
Rashmita Baraskar <rashmitabaraskar11@gmail.com>
2023-06-22 16:00:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+gsap.to("#bottal",{
+    scrollTrigger:{
+        trigger:"#bottal",
+        start:"top 15.9%",
+        end:"top -400%",
+        pin:true,
+        scrub:2,
+    },
+    rotate:"-15deg"
+})
+gsap.to("#bottal",{
+    scrollTrigger:{
+        trigger:"#bottal",
+        start:"top 0%",
+        end:"top 0%",
+        pin:true,
+        scrub:2,
+        
+    },
+    opacity:0
+
+})
